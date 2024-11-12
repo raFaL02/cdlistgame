225954c035ac9b91d67c2b359d343aba01551b52
Rafael de Lima <rafalimabcl@gmail.com>
2024-11-11 21:44:11 -0300

    feat: initial commit

---
+++ src/main/java/com/codecraft/cdlist/CdlistApplication.java
@@ -0,0 +1,13 @@
+package com.codecraft.cdlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecraft/cdlist/CdlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecraft.cdlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
