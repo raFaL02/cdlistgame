a82a6fd48a535ebdf6fd06bb96eeab226f896428
Rafael de Lima <rafalimabcl@gmail.com>
2024-11-14 23:06:53 -0300

    feat: add game DTO, get game by id

--- src/main/java/com/codecraft/cdlist/services/GameService.java
+++ src/main/java/com/codecraft/cdlist/services/GameService.java
@@ -1,9 +1,12 @@
 package com.codecraft.cdlist.services;
 
+import com.codecraft.cdlist.dto.GameDTO;
 import com.codecraft.cdlist.dto.GameMinDTO;
+import com.codecraft.cdlist.entities.Game;
 import com.codecraft.cdlist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAllGames() {
         return gameRepository.findAll().stream().map(game -> new GameMinDTO(game)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).orElse(null);
+        return new GameDTO(result);
     }
 }
